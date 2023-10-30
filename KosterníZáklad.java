10129c70296cd7b40b934c495b15c9b76f45df48
BrutalikCZ <tomas.brezina2006@gmail.com>
2023-10-30 17:04:17 +0100

    JÁ POSRAR

---
+++ KosterníZáklad.java
@@ -0,0 +1,26 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int rokNarozeni;
+        int cilovyVek;
+        int vysledek;
+        int cisloDni;
+        int zaDni;
+
+
+        
+
+
+
+
+
+
+
+    }
+}
