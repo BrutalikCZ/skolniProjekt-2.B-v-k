ce3a95024b8f46c052212305df505c579d67c383
Matesvlk <vlkpce1@gmail.com>
2023-10-30 14:28:26 +0100

    xd

--- soubor.java
+++ soubor.java
@@ -3,12 +3,36 @@ import java.util.Scanner;
 import java.text.SimpleDateFormat;
 import java.text.ParseException;
 import java.util.Date;
-int vyan;
-int paddy;
-paddy = 11;
-vyan = 12;
-if (vyan - paddy) > 0
-System.out.println("Nice pb lol");
+import java.util.HashMap;
+import java.util.Map;
 
+public class Main {
+    public static void main(String[] args) {
+        Map<String, Integer> mesiceNaDny = new HashMap<>();
+        mesiceNaDny.put("leden", 31);
+        mesiceNaDny.put("unor", 28); 
+        mesiceNaDny.put("brezen", 31);
+        mesiceNaDny.put("duben", 30);
+        mesiceNaDny.put("kveten", 31);
+        mesiceNaDny.put("cerven", 30);
+        mesiceNaDny.put("cervenec", 31);
+        mesiceNaDny.put("srpen", 31);
+        mesiceNaDny.put("zari", 30);
+        mesiceNaDny.put("rijen", 31);
+        mesiceNaDny.put("listopad", 30);
+        mesiceNaDny.put("prosinec", 31);
+
+        
+        String nazevMesice = "leden";
+
+        
+        int pocetDni = mesiceNaDny.get(nazevMesice);
+
+       
+        System.out.println("Měsíc " + nazevMesice + " má " + pocetDni + " dní.");
+    }
+}
+l");
+ 
 
 
