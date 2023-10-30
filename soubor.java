6b8e6e44625a4b8a096fca02f1d71bf5fc77f49c
Matesvlk <vlkpce1@gmail.com>
2023-10-30 14:20:34 +0100

    vyan

--- soubor.java
+++ soubor.java
@@ -1,7 +1,14 @@
-int a;
-a = 12;
-if (a > 3) {
-    System.out.println("ahoj")
-}
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+import java.util.Date;
+int vyan;
+int paddy;
+paddy = 11;
+vyan = 12;
+if (vyan-paddy) = > 0
+System.out.println("Nice pb lol");
+
 
 
