764bf18511071ea79b69a7b5054e7447cc470d9a
BrutalikCZ <tomas.brezina2006@gmail.com>
2023-11-04 14:35:25 +0100

    Začátek Všeobecných Prací (Priorita - VELKÁ)

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int rokNarozeni;
+        int cilovyVek; //Kolik let uživatel chce, aby mu bylo
+        int cisloDni; //Výsledek metody, která převede měsíce na dny a součet se dnem uloží do proměnné
+        int zaDni; //Za jak dlouho budou další narozeniny
+        int dnesniDatum; //Dnešní datum
+        int jeTiLet; //Kolik je danému člověku LET
+
+
+        
+
+
+
+        
+        jeTiLet = rokNarozeni - cilovyVek;
+
+
+
+        System.out.println("Je ti" + (dnesniDatum - datumNarozeni) );
+
+
+    }
+}
